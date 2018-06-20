edaa2b82343deacf2a72c8cfd2512f4278a2bd57
1129213050@qq.com <lax123456>
2018-06-21 00:21:19 +0800

    目录结构调整

--- leiax00/basic/ReplaceTools/start_config.py
+++ leiax00/basic/ReplaceTools/start_config.py
@@ -3,20 +3,15 @@
 import logging
 import os
 
+# **********************************系统属性配置*************************************
 PROJECT_ROOT = r'F:\work_space\pythonSpace\leiax00\basic\ReplaceTools'
-LOGGING_PATH = os.path.join(PROJECT_ROOT, r'temp', r'log', r'log.log')
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s [%(levelname)s] %(message)s (%(filename)s line:%(lineno)d)',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    filename=LOGGING_PATH,
-                    filemode='w')
 DB_TYPE = {
     'mysql': 'mysql',
     'oracle': 'oracle',
 }
-
+# **********************************环境属性配置*************************************
 CONFIG = {
-    'root_path': r'F:\work_space\pythonSpace\leiax00\basic\ReplaceTools\root_path',
+    'root_path': r'F:\work_space\pythonSpace\leiax00\basic\ReplaceTools\temp\root_path',
     'db_config': {
         'db_type': DB_TYPE['mysql'],
         'ip': r'localhost',
@@ -26,5 +21,10 @@ CONFIG = {
         'passwd': r'root',
     }
 }
-
-print(LOGGING_PATH)
+# **********************************日志属性配置*************************************
+LOGGING_PATH = os.path.join(PROJECT_ROOT, r'temp', r'log', r'log.log')
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s [%(levelname)s] %(message)s (%(filename)s line:%(lineno)d)',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename=LOGGING_PATH,
+                    filemode='w')
