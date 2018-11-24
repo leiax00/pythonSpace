5e2bb92f1859d1a26b62503688f7d49b90d04673
1129213050@qq.com <lax123456>
2018-11-24 17:29:34 +0800

    爬虫demo.....

---
+++ leiax00/scrapydemo/demo01/demo01/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scraptest
+
+
+class Demo01Item(scraptest.Item):
+    # define the fields for your item here like:
+    # name = scrapydemo.Field()
+    pass
---
+++ leiax00/scrapydemo/demo01/demo01/spiders/book_spider.py
@@ -0,0 +1,28 @@
+# encoding:utf-8
+import os
+
+import scrapy
+import scrapy.cmdline
+
+
+class BookSpider(scrapy.Spider):
+    name = 'books'
+    start_urls = ['http://books.toscrape.com/']
+
+    def parse(self, response):
+        for book in response.css('article.product_pod'):
+            name = book.xpath('./h3/a/@title').extract_first()
+            price = book.css('p.price_color::text').extract_first()
+            yield {
+                'name': name,
+                'price': price,
+            }
+        next_url = response.css('ul.pager li.next a::attr(href)').extract_first()
+        if next_url:
+            next_url = response.urljoin(next_url)
+            yield scrapy.Request(next_url, callback=self.parse)
+
+
+if __name__ == '__main__':
+    file_path = os.path.join('..', 'temp', 'books.csv')
+    scrapy.cmdline.execute(['scrapy', 'crawl', 'books', '-o', file_path])
